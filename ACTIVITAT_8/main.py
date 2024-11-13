282a4c3ce6cb8ba21ae1d6433c671bedc2783932
Arturznak <2023_artur.znakomskyy@iticbcn.cat>
2024-11-13 20:49:35 +0100

    Commit clase(response)

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float 
+    size: float 
+    color: str
     
 app = FastAPI()
 
-@app.get("/items/{item_id}")
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.get("/items/{item_id}", status_code=201)
 async def read_item(item_id: int):
     return {"item_id": item_id }
